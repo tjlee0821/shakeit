3398b4ad1fca6f71fb9d3ab8b635b21156a9beb6
Taeryong Lee <teo0821@gmail.com>
2023-08-20 10:19:59 +0900

    step-1

---
+++ lib/my_home_page.dart
@@ -0,0 +1,94 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  //mixin
+  int _counter = 0;
+
+  late ShakeDetector _detector;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    _detector = ShakeDetector.autoStart(
+      onPhoneShake: () {
+        setState(
+          () {
+            _counter++;
+          },
+        );
+      },
+      shakeThresholdGravity: 1.5,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ).pSymmetric(h: 20, v: 50),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes au  to-formatting nicer for build methods.
+    );
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.detached:
+        break;
+      case AppLifecycleState.resumed:
+        _detector.startListening();
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.hidden:
+        break;
+      case AppLifecycleState.paused:
+        _detector.stopListening();
+        break;
+    }
+  }
+}
